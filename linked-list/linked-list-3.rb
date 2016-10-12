bf876b65a1b8fb1ed71b0d2cd15f5038e8f563a7
Adam D <addstar34@gmail.com>
2016-10-12 15:54:40 +1000

    linked list floyds tortose and hare infinite loop cycle detection

---
+++ linked-list/linked-list-3.rb
@@ -0,0 +1,44 @@
+class LinkedListNode
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+def print_values(list_node)
+  if list_node
+    print "#{list_node.value} --> "
+    print_values(list_node.next_node)
+  else
+    print "nil\n"
+    return
+  end
+end
+
+def infinite_loop?(list)
+  tortoise = list.next_node
+  hare = list.next_node
+
+  until hare.nil?
+    hare = hare.next_node
+    return false if hare.nil?
+
+    hare = hare.next_node
+    tortoise = tortoise.next_node
+    return true if hare == tortoise
+  end
+
+  return false
+end
+
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+node4 = LinkedListNode.new(45, node3)
+node5 = LinkedListNode.new(21, node4)
+
+puts infinite_loop?(node5)
+node1.next_node = node5 # creates an infinite loop
+puts infinite_loop?(node5)
